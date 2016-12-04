351b8f30b16a1a53863dcedf1659cb2c8e355db7
Josh <josh@eastfive.com>
2016-12-03 21:41:41 -0600

    removed some depricated Update methods and added LookupDocument as a common type

---
+++ LookupDocument.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBarLabs.Persistence.Azure.Documents
+{
+    [Serializable]
+    [DataContract]
+    public class LookupDocument : TableEntity
+    {
+        [IgnoreDataMember]
+        [IgnoreProperty]
+        public Guid Id { get { return Guid.Parse(this.RowKey); } }
+
+        public Guid Lookup { get; set; }
+    }
+}
