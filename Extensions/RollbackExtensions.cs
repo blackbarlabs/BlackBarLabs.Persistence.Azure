d115c87eb1b0a45dbac0057989f53efc914b9035
Josh <josh@eastfive.com>
2016-12-22 12:39:16 -0600

    added new rollback extension

--- Extensions/RollbackExtensions.cs
+++ Extensions/RollbackExtensions.cs
@@ -46,6 +46,50 @@ namespace BlackBarLabs.Persistence.Azure
                 });
         }
 
+        private struct Carry<T>
+        {
+            public T carry;
+        }
+
+        public static void AddTaskUpdate<T, TRollback, TDocument>(this RollbackAsync<TRollback> rollback,
+            Guid docId,
+            Func<TDocument, T> mutateUpdate,
+            Func<T, TDocument, bool> mutateRollback,
+            Func<TRollback> onNotFound,
+            AzureStorageRepository repo)
+            where TDocument : class, ITableEntity
+        {
+            rollback.AddTask(
+                async (success, failure) =>
+                {
+                    var r = await repo.UpdateAsync<TDocument, Carry<T>?>(docId,
+                        async (doc, save) =>
+                        {
+                            var carry = mutateUpdate(doc);
+                            await save(doc);
+                            return new Carry<T>
+                            {
+                                carry = carry,
+                            };
+                        },
+                        () => default(Carry<T>?));
+                    if (r.HasValue)
+                        return success(
+                            async () =>
+                            {
+                                await repo.UpdateAsync<TDocument, bool>(docId,
+                                    async (doc, save) =>
+                                    {
+                                        mutateRollback(r.Value.carry, doc);
+                                        await save(doc);
+                                        return true;
+                                    },
+                                    () => false);
+                            });
+                    return failure(onNotFound());
+                });
+        }
+
         public static void AddTaskCreate<TRollback, TDocument>(this RollbackAsync<TRollback> rollback,
             Guid docId, TDocument document,
             Func<TRollback> onAlreadyExists,
