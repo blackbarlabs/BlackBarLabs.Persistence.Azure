b99cca4a6b203634695e833a8770e0c8b118b1b2
Josh Wingstrom <josh@eastfive.com>
2018-03-19 16:08:30 -0500

    merged out stupidy

--- StorageTables/AzureStorageRepository.Admin.cs
+++ StorageTables/AzureStorageRepository.Admin.cs
@@ -15,15 +15,6 @@ namespace BlackBarLabs.Persistence.Azure.StorageTables
             await Task.WhenAll(deleteTasks);
             return true;
         }
-
-        public Task<Task> FindLinkedDocumentsAsync<T1, T2, T3>(Guid actorId, string systemName, Func<T1, Guid[]> p1, Func<object, object, Task<object>> p2, Func<Task<object>> p3)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<TResult> LockedUpdateAsync<T1, TResult>(Guid credentialId, Func<T1, bool> p1, Func<T1, object, object, Task<bool>> p2, Func<TResult> onNotFound, Func<TResult> onLockRejected)
-        {
-            throw new NotImplementedException();
-        }
+        
     }
 }
